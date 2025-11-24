3f800351e542b8a4cde94f07485f971aca63b86a
Stepan Grigoryev <grigoriev.stepka@yandex.ru>
2025-11-24 19:18:53 +0500

    repositories designed

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -1,5 +1,5 @@
 from typing import AsyncGenerator, Annotated
-
+from fastapi import Depends
 from app.core.database import session_maker
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.repositories.user_repository import Repository as UserRepository
@@ -12,7 +12,7 @@ async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
 
 
 async def get_user_repository(
-    db: Annotated[AsyncSession, get_async_session],
+    db: Annotated[AsyncSession, Depends(get_async_session)],
 ) -> UserRepository:
     return UserRepository(db=db)
 
