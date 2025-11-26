dfb6b06de5e89334d75a1807f8458b1d0c0a4956
Stepan Grigoryev <grigoriev.stepka@yandex.ru>
2025-11-26 20:01:17 +0500

    changes

--- app/repositories/friendship_repository.py
+++ app/repositories/friendship_repository.py
@@ -1,47 +1,42 @@
 import uuid
-from typing import Optional, List
-from sqlalchemy import select, or_, and_, delete
+from typing import Optional
+
+from sqlalchemy import select, or_, and_
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
+
 from app.models.friendship import Friendship
 from app.models.friendship_status import FriendshipStatus
+from app.repositories.base_repository import BaseRepo
 
 
-class Repository:
+class FriendshipRepository(BaseRepo[Friendship]):
     def __init__(self, db: AsyncSession):
-        self.db = db
+        super().__init__(Friendship, db)
 
-    async def create_friendship(self, friendship: Friendship) -> Friendship:
-        self.db.add(friendship)
-        await self.db.commit()
-        await self.db.refresh(friendship)
-        return friendship
-
-    async def get_friendship_between(self, user_id_1: uuid.UUID, user_id_2: uuid.UUID) -> Optional[Friendship]:
-        query = select(Friendship).where(or_(
-                and_(Friendship.sender_id == user_id_1, Friendship.receiver_id == user_id_2),
-                and_(Friendship.sender_id == user_id_2, Friendship.receiver_id == user_id_1)
+    async def get_status_between(self, user_id_1: uuid.UUID, user_id_2: uuid.UUID) -> Optional[Friendship]:
+        query = select(self.model).where(or_(
+                and_(self.model.sender_id == user_id_1, self.model.receiver_id == user_id_2),
+                and_(self.model.sender_id == user_id_2, self.model.receiver_id == user_id_1)
             )
         )
-        result = await self.db.execute(query)
-        return result.scalars().first()
+        return await self.db.scalar(query)
 
-    async def get_pending_requests(self, user_id: uuid.UUID) -> List[Friendship]:
-        query = select(Friendship).where(
-            (Friendship.receiver_id == user_id) & (Friendship.status == FriendshipStatus.PENDING)
-        ).options(selectinload(Friendship.sender))
-        result = await self.db.execute(query)
-        return list(result.scalars().all())
+    async def get_pending_requests(self, user_id: uuid.UUID) -> list[Friendship]:
+        query = (select(self.model).where(
+            (self.model.receiver_id == user_id) & (self.model.status == FriendshipStatus.PENDING))
+                 .options(selectinload(self.model.sender)))
+        await self.db.execute(query)
+        return self.db.scalars().all()
 
-    async def update_status(self, friendship: Friendship, new_status: FriendshipStatus) -> Friendship:
-        friendship.status = new_status
-        self.db.add(friendship)
-        await self.db.commit()
-        await self.db.refresh(friendship)
-        return friendship
 
-    async def delete_friendship(self, friendship_id: uuid.UUID) -> bool:
-        query = delete(Friendship).where(Friendship.id == friendship_id)
-        result = await self.db.execute(query)
-        await self.db.commit()
-        return result.rowcount > 0
+
+    async def get_friends_list(self, user_id: uuid.UUID) -> list[Friendship]:
+        query = select(self.model).where(
+            ((self.model.sender_id == user_id) | (self.model.receiver_id == user_id)) &
+            (self.model.status == FriendshipStatus.ACCEPTED)
+        ).options(selectinload(self.model.sender), selectinload(self.model.receiver))
+        await self.db.execute(query)
+        return self.db.scalars().all()
+
+
