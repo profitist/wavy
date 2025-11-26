f907e6e4f51415d5839894d5fee73a49d59e0d93
Stepan Grigoryev <grigoriev.stepka@yandex.ru>
2025-11-26 20:01:17 +0500

    changes

--- app/repositories/reaction_repository.py
+++ app/repositories/reaction_repository.py
@@ -1,36 +1,27 @@
 import uuid
-from typing import Optional, List
+from typing import List, Optional
 
-from sqlalchemy import select, delete
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
+
 from app.models.reactions import Reaction
+from app.repositories.base_repository import BaseRepo
 
-class Repository:
+
+class ReactionRepository(BaseRepo[Reaction]):
     def __init__(self, db: AsyncSession):
-        self.db = db
-
-    async def add_reaction(self, reaction: Reaction) -> Reaction:
-        self.db.add(reaction)
-        await self.db.commit()
-        await self.db.refresh(reaction)
-        return reaction
+        super().__init__(Reaction, db)
 
     async def get_by_user_and_share(self, user_id: uuid.UUID, share_id: uuid.UUID) -> Optional[Reaction]:
-        query = select(Reaction).where((Reaction.user_id == user_id) &
-                                       (Reaction.shared_track_id == share_id))
-        result = await self.db.execute(query)
-        return result.scalars().first()
+        query = select(self.model).where((self.model.user_id == user_id) &
+                                         (self.model.shared_track_id == share_id))
+        return await self.db.scalar(query)
 
-    async def get_reactions_for_share(self, share_id: uuid.UUID) -> List[Reaction]:
-        query = select(Reaction)\
-            .where(Reaction.shared_track_id == share_id)\
-            .options(selectinload(Reaction.user)) # Подгрузить, КТО поставил
+    async def get_for_share(self, share_id: uuid.UUID) -> List[Reaction]:
+        query = select(self.model) \
+            .where(self.model.shared_track_id == share_id) \
+            .options(selectinload(self.model.user))
+
         result = await self.db.execute(query)
         return list(result.scalars().all())
-
-    async def remove_reaction(self, reaction_id: uuid.UUID) -> bool:
-        query = delete(Reaction).where(Reaction.id == reaction_id)
-        result = await self.db.execute(query)
-        await self.db.commit()
-        return result.rowcount > 0
--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -1,51 +1,19 @@
-import uuid
-
-from typing import Annotated
-from sqlalchemy.ext.asyncio import AsyncSession
+from typing import Optional
 from sqlalchemy import select
-from app.core.dependencies import get_async_session
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from app.models.user import User
+from app.repositories.base_repository import BaseRepo
 
 
-class Repository:
-    """
-    Репозиторий работы с бд для юзера / степа тут надо
-    написать все запросики через self.db
-    """
+class UserRepository(BaseRepo[User]):
+    def __init__(self, db: AsyncSession):
+        super().__init__(User, db)
 
-    def __init__(self, db: Annotated[AsyncSession, get_async_session]):
-        self.db = db
+    async def get_by_username(self, username: str) -> Optional[User]:
+        query = select(self.model).where(self.model.username == username)
+        return await self.db.scalar(query)
 
-    async def get_user_by_uuid(self, user_id: uuid.UUID) -> User:
-        query = select(User).where(User.id == user_id)
-        result = await self.db.execute(query)
-        return result.scalars().first()
-
-    async def get_user_by_username(self, username: str) -> User:
-        query = select(User).where(User.username == username)
-        result = await self.db.execute(query)
-        return result.scalars().first()
-
-    async def create_user(self, user: User) -> User:
-        self.db.add(user)
-        try:
-            await self.db.commit()
-            await self.db.refresh(user)
-            return user
-        except Exception as e:
-            await self.db.rollback()
-            raise e
-
-    async def update_user(self, user: User) -> User:
-        self.db.add(user)
-        await self.db.commit()
-        await self.db.refresh(user)
-        return user
-
-    async def delete_user(self, user_id: uuid.UUID) -> bool:
-        user = await self.get_user_by_uuid(user_id)
-        if user:
-            await self.db.delete(user)
-            await self.db.commit()
-            return True
-        return False
+    async def get_by_email(self, email: str) -> Optional[User]:
+        query = select(self.model).where(self.model.email == email)
+        return await self.db.scalar(query)
