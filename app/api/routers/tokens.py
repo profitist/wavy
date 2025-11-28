54cd0e6f9ac4f1904fcb0c26ba06a5db286c0823
Ivan Mikhaylov <ivanmikhaylov.sesc@gmail.com>
2025-11-28 23:33:49 +0500

    auth block done + refactoring хз ываще че написал)) СМОТРИ фалы tokens.py and auth.py Остальное рефакторинг

--- app/api/routers/tokens.py
+++ app/api/routers/tokens.py
@@ -1,9 +1,12 @@
 from typing import List, Annotated
 
-from fastapi import APIRouter, Depends
+import jwt
+from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from app.core.dependencies import get_user_service
 from app.services.user_service import Service as UserService
+from app.auth.auth import verify_password, create_access_token, create_refresh_token
+from app.config import JWT_SECRET_KEY, ALGORITHM
 
 router = APIRouter()
 
@@ -13,5 +16,47 @@ async def login(
     user_service: Annotated[UserService, Depends(Depends(get_user_service))],
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
 ):
-    return await user_service.login_user(form_data)
-    # TO DO
+    db_user = await user_service.get_by_name(form_data.username)
+    if not db_user or not verify_password(db_user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token = create_access_token(
+        data={"sub": db_user.username, "role": db_user.role, "id": db_user.id}
+    )
+    refresh_token = create_refresh_token(
+        data={"sub": db_user.username, "role": db_user.role, "id": db_user.id}
+    )
+    return {
+        "access_token": access_token,
+        "refresh_token": refresh_token,
+        "token_type": "bearer",
+    }
+
+
+@router.post("/refresh")
+async def refresh_token(
+    refreshed_token: str,
+    user_service: Annotated[UserService, Depends(get_user_service)],
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate refresh token",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(refreshed_token, JWT_SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except jwt.PyJWTError:
+        raise credentials_exception
+    db_user = await user_service.get_by_name(username)
+    if db_user is None:
+        raise credentials_exception
+    access_token = create_access_token(
+        data={"sub": db_user.username, "role": db_user.role, "id": db_user.id}
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
