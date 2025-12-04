8b8b0a696655b424ee9308853b77b05015044d1d
Ivan Mikhaylov <ivanmikhaylov.sesc@gmail.com>
2025-12-04 14:23:59 +0500

    edit main

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from app.api.routers import friendship
 
 app = FastAPI()
 
+app.include_router(friendship.router)
 app.include_router(user.router)
 app.include_router(tokens.router)
 app.include_router(track.router)
