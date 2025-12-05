04e226f2663dd3eef2391637e839ab6eb9dfaa4f
Ivan Mikhaylov <ivanmikhaylov.sesc@gmail.com>
2025-12-05 20:58:26 +0500

    add s3 ctorage client

---
+++ app/core/s3.py
@@ -0,0 +1,50 @@
+from contextlib import asynccontextmanager
+
+from aiobotocore.session import get_session
+from fastapi import HTTPException
+from starlette import status
+
+
+class S3Client:
+    def __init__(
+            self,
+            access_key: str,
+            secret_key: str,
+            endpoint_url: str,
+            bucket_name: str,
+    ):
+        self.config = {
+            "aws_access_key_id": access_key,
+            "aws_secret_access_key": secret_key,
+            "endpoint_url": endpoint_url,
+        }
+        self.bucket_name = bucket_name
+        self.session = get_session()
+
+    @asynccontextmanager
+    async def get_client(self):
+        async with self.session.create_client("s3", **self.config) as client:
+            yield client
+
+    async def upload_file(
+            self,
+            file_path: str,
+    ):
+        object_name = file_path.split("/")[-1]  # /users/artem/cat.jpg
+        try:
+            async with self.get_client() as client:
+                with open(file_path, "rb") as file:
+                    await client.put_object(
+                        Bucket=self.bucket_name,
+                        Key=object_name,
+                        Body=file,
+                    )
+        except Exception as e:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="image can't be uploaded",
+            )
+
+
+
+
