b5235f049d8cc1c182116a51c7f86ec45e4795ba
Ivan Mikhaylov <ivanmikhaylov.sesc@gmail.com>
2025-12-11 13:18:31 +0500

    add middleware

--- main.py
+++ main.py
@@ -4,8 +4,19 @@ from app.api.routers import track
 from app.api.routers import tokens
 from app.api.routers import friendship
 from app.api.routers import shared_track
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(friendship.router)
 app.include_router(user.router)
