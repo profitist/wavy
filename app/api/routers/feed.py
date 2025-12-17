4e237c820f37568c8bb927e666e728f4b095e48b
Stepan Grigoryev <grigoriev.stepka@yandex.ru>
2025-12-17 20:44:07 +0500

    change feed

--- app/api/routers/feed.py
+++ app/api/routers/feed.py
@@ -1,11 +1,7 @@
-from typing import Annotated
-from fastapi import APIRouter, Depends
-
-from app.core.dependencies import get_sharing_service
+from fastapi import APIRouter
+import uuid
 from app.schemas.shared_track_schema import SharedTrackResponseSchema
-from app.services.sharing_service import SharingService
-from app.models.user import User
-from app.auth.user_validation import get_current_user
+from app.models.shared_track import SharedTrack
 
 
 router = APIRouter(prefix="/feed", tags=["Feed"])
@@ -13,13 +9,17 @@ router = APIRouter(prefix="/feed", tags=["Feed"])
 
 @router.get("/", response_model=list[SharedTrackResponseSchema])
 async def get_my_feed(
-    service: Annotated[SharingService, Depends(get_sharing_service)],
-    current_user: Annotated[User, Depends(get_current_user)],
-    limit: int = 20,
-    offset: int = 0,
-):
-    return await service.get_feed_for_user(
-        user_id=current_user.id,
-        limit=limit,
-        offset=offset,
+        self, user_id: uuid.UUID, limit: int = 20, offset: int = 0
+) -> list[SharedTrack]:
+    friends = await self.friend_repo.get_friends_list(user_id)
+    ids = []
+    for relation in friends:
+        if relation.sender_id == user_id:
+            ids.append(relation.receiver_id)
+        else:
+            ids.append(relation.sender_id)
+    if not ids:
+        return []
+    return await self.share_repo.get_last_tracks_feed(
+        user_ids=ids, limit=limit, offset=offset
     )
